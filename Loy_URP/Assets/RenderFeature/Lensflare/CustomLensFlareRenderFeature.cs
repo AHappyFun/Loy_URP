4d9826d2ed0b8ead519bc62f1e33f9d17d2b10e5
ALey_Z <54310685+AHappyFun@users.noreply.github.com>
2025-10-31 15:26:21 +0800

    加lensflare

---
+++ Loy_URP/Assets/RenderFeature/Lensflare/CustomLensFlareRenderFeature.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
+
+/// <summary>
+/// 自定义阳光LensFlare效果
+/// </summary>
+public class CustomLensFlareRenderFeature : ScriptableRendererFeature
+{
+    private Material material;
+    private CustomLensFlareRenderPass renderPass;
+
+    public Shader shader;
+
+    public List<float> FlareIntensity;
+    public List<float> FlareScale;
+    public List<float> FlareOffset;
+    public int FlareLayerCount;
+
+    public Texture2D[] FlareTexs;
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (material == null)
+        {
+            material = CoreUtils.CreateEngineMaterial(shader);
+        }
+
+        if (material == null) return;
+
+        renderer.EnqueuePass(renderPass);
+    }
+    public override void Create()
+    {
+        if(renderPass == null)
+            renderPass = new CustomLensFlareRenderPass(this);
+
+        shader = Shader.Find("Loy/Feature/CustomLensFlare");
+
+    }
+
+    class CustomLensFlareRenderPass : ScriptableRenderPass
+    {
+        const string m_ProfilerTag = "Loy_CustomLensFlare";
+        ProfilingSampler m_ProfilingSampler;
+
+        private CustomLensFlareRenderFeature renderFeature;
+        public Vector4 FlarePos0;
+        public Vector4 FlarePos1;
+        public Vector4 FlarePos2;
+        public Vector4 FlarePos3;
+
+        public Vector4[] FlareParams; //xy pos z scale w intensity
+
+
+        public CustomLensFlareRenderPass(CustomLensFlareRenderFeature renderFeature)
+        {
+            m_ProfilingSampler = new ProfilingSampler(m_ProfilerTag);
+            this.renderFeature = renderFeature;
+            this.renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+            FlareParams = new Vector4[4];
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+
+            //主光
+            Light sun = RenderSettings.sun;
+            //
+            if(sun == null)
+                return;
+
+            //自定义光源
+            //var lens = DistortManager.Instance.LensFlareInstances;
+            //var len = lens[0];
+
+            Camera cam = renderingData.cameraData.camera;
+            Vector3 worldPos = cam.transform.position + sun.transform.forward * -cam.farClipPlane * 0.99f;
+
+            Vector3 viewPos = cam.worldToCameraMatrix.MultiplyPoint(worldPos);
+
+            Vector3 sunScreenPos = cam.WorldToViewportPoint(worldPos);
+
+            if (sunScreenPos.x < 0.0f || sunScreenPos.y < 0.0f || sunScreenPos.x > 1 || sunScreenPos.y > 1)
+            {
+                return;
+            }
+
+            // 👇 检查是否在摄像机前方（z > 0）
+            if (viewPos.z > 0f)
+            {
+                return;
+            }
+
+            Vector3 screenPos = cam.WorldToViewportPoint(worldPos);
+            Vector2 screenCenter = new Vector2(0.5f, 0.5f);
+            Vector2 dir = screenCenter - new Vector2(screenPos.x, screenPos.y);
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                float offsetV = renderFeature.FlareOffset[i];
+                Vector2 viewport = new Vector2(screenPos.x, screenPos.y) + dir * offsetV;
+                FlareParams[i] = new Vector4(viewport.x, viewport.y, renderFeature.FlareScale[i], renderFeature.FlareIntensity[i]);
+            }
+
+            CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
+            using (new ProfilingScope(cmd, m_ProfilingSampler))
+            {
+                for (int i = 0; i < renderFeature.FlareLayerCount; i++)
+                {
+                    renderFeature.material.SetTexture("_FlareTex" + i, renderFeature.FlareTexs[i]);
+                }
+
+                renderFeature.material.SetVectorArray("_FlareParams", FlareParams);
+                renderFeature.material.SetInt("_FlareLayerCount", renderFeature.FlareLayerCount);
+                renderFeature.material.SetVector("_FlareSunPos", sunScreenPos);
+                renderFeature.material.SetFloat("_ScreenAspect", (float)cam.pixelHeight / (float)cam.pixelWidth);
+
+                cmd.DrawProcedural(Matrix4x4.identity, renderFeature.material, 0, MeshTopology.Triangles, 3, 1);
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+
+            }
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+}
---
+++ Loy_URP/Assets/RenderFeature/Lensflare/Loy_CustomLensFlare.shader
@@ -0,0 +1,116 @@
+﻿Shader "Loy/Feature/CustomLensFlare"
+{
+    Properties
+    {
+        //_Intensity("Intensity", Float) = .1
+        //_Falloff("Falloff", Float) = 8
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "Queue"="Overlay" }
+
+       Pass
+        {
+            Name "CustomLensFlare"
+            ZTest Always Cull Off ZWrite Off
+            Blend One One
+
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
+            
+            int _FlareLayerCount;
+            float4 _FlareParams[4];
+            float4 _FlareSunPos;
+            float _ScreenAspect;
+            
+            TEXTURE2D(_FlareTex0);
+            TEXTURE2D(_FlareTex1);
+            TEXTURE2D(_FlareTex2);
+            TEXTURE2D(_FlareTex3);
+            SAMPLER(sampler_FlareTex0);
+            
+            struct Attributes
+            {
+                uint vertexID   : SV_VertexID;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct Varyings
+            {
+                float4 positionCS : SV_POSITION;
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+
+            Varyings vert(Attributes IN)
+            {
+                Varyings OUT;
+                UNITY_SETUP_INSTANCE_ID(input);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+                
+                OUT.positionCS = GetFullScreenTriangleVertexPosition(IN.vertexID);
+
+                return OUT;
+            }
+
+            //float3 hsv2rgb(float3 c)
+            //{
+            //    float4 K = float4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+            //    float3 p = abs(frac(c.xxx + K.xyz) * 6.0 - K.www);
+            //    return c.z * lerp(K.xxx, saturate(p - K.xxx), c.y);
+            //}
+            
+            float4 flare3(float2 uv, float2 center, float scale, Texture2D flareTex, float intensity)
+            {
+                float2 localUV = (uv - center) * float2(1.0f, _ScreenAspect) / scale + 0.5;
+                if (any(localUV < 0.0) || any(localUV > 1.0)) return 0;
+
+                float4 texColor = SAMPLE_TEXTURE2D(flareTex, sampler_FlareTex0, localUV);
+                texColor.rgb *= intensity;
+                return texColor * texColor.a;
+            }
+
+            float2 getOffset(float2 flarePos, float2 screenCenter, float offsetScale)
+            {
+                float2 dir = normalize(screenCenter - flarePos);
+                return dir * offsetScale;
+            }
+
+            float LinearEyeDepth(float rawDepth)
+            {
+                return Linear01Depth(rawDepth, _ZBufferParams) * _ProjectionParams.z;
+            }
+            
+            half4 frag(Varyings IN) : SV_Target
+            {
+
+                UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(IN);
+                
+                float2 uv = IN.positionCS.xy / _ScaledScreenParams.xy;
+                half4 col = 0;
+
+                float2 sunUV = _FlareSunPos.xy;
+                float Z = SampleSceneDepth(sunUV);
+            #if !UNITY_REVERSED_Z
+                Z = lerp(UNITY_NEAR_CLIP_VALUE, 1, Z);
+            #endif
+
+                //太阳前面有东西挡住，不显示LensFlare
+                float linearDepth = LinearEyeDepth(Z);
+                if(linearDepth < 999.0f)
+                    return 0;
+                
+                if (_FlareLayerCount > 0) col += flare3(uv, _FlareParams[0].xy, _FlareParams[0].z, _FlareTex0, _FlareParams[0].w);
+                if (_FlareLayerCount > 1) col += flare3(uv, _FlareParams[1].xy, _FlareParams[1].z, _FlareTex1, _FlareParams[1].w);
+                if (_FlareLayerCount > 2) col += flare3(uv, _FlareParams[2].xy, _FlareParams[2].z, _FlareTex2, _FlareParams[2].w);
+                if (_FlareLayerCount > 3) col += flare3(uv, _FlareParams[3].xy, _FlareParams[3].z, _FlareTex3, _FlareParams[3].w);
+                return col;
+            }
+
+            ENDHLSL
+        }
+    }
+}
